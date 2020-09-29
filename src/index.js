c1fcc029bbb71a216c011eef0bef08fc9dba13c1
Aliaksei_Akulau <akulaualiaksei@gmail.com>
2020-09-29 21:24:54 +0300

    feat: implement min, max and avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (Array.isArray (array) && array.length > 0) {
+        return Math.min.apply(this, array);
+    } else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (Array.isArray (array) && array.length > 0) {
+        return Math.max.apply(this, array);
+    } else {
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (Array.isArray (array) && array.length > 0) {
+        let summArray = array.reduce(function(sum, current){
+            return sum + current;}, 0);
+        return summArray / array.length;
+    } else {
+        return 0;
+    }
 }
